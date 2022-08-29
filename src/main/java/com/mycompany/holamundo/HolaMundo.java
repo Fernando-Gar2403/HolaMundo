7f1d517db193885b3627a30da6425fdb7b6a7723
FernandoGarcia <fernandoproyects@gmail.com>
2022-08-29 13:38:47 -0600

    Primer commit

---
+++ src/main/java/com/mycompany/holamundo/HolaMundo.java
@@ -0,0 +1,18 @@
+/*Licencia de uso*/
+
+package com.mycompany.holamundo;
+
+/*Autor@fernando */
+
+public class HolaMundo { 
+  public static void main (String[ ] args) {
+      System.out.println("Hola Mundo"); //operaciones
+  }
+    //Una funcion, en java llamada clase esta compuesto por el nombre de la funcion
+   //y luego entre parentesis los tipos de datos que contendra la logica de programacion
+   //dentro de la funcion
+
+
+
+
+}
